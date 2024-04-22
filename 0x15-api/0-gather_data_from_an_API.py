05fe4534d00e448af2cd87e52b01522d268762e1
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2024-04-22 09:53:30 -0700

    rosa

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,35 +15,21 @@ if __name__ == "__main__":
     # Base URL for the JSONPlaceholder API
     url = "https://jsonplaceholder.typicode.com/"
 
-    try:
-        # Get the employee information using the provided employee ID
-        employee_id = sys.argv[1]
-        user_response = requests.get(url + "users/{}".format(employee_id))
+    # Get the employee information using the provided employee ID
+    employee_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(employee_id)).json()
 
-        # Check if request was successful
-        user_response.raise_for_status()
-        user = user_response.json()
+    # Get the to-do list for the employee using the provided employee ID
+    params = {"userId": employee_id}
+    todos = requests.get(url + "todos", params=params).json()
 
-        # Get the to-do list for the employee using the provided employee ID
-        params = {"userId": employee_id}
-        todos_response = requests.get(url + "todos", params=params)
+    # Filter completed tasks and count them
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
 
-        # Check if request was successful
-        todos_response.raise_for_status()
-        todos = todos_response.json()
+    # Print the employee's name and the number of completed tasks
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
 
-        # Filter completed tasks and count them
-        completed = [t.get("title") for t in todos if t.get("completed")]
-
-        # Print the employee's name and the number of completed tasks
-        print("Employee {} is done with tasks ({}/{}):".format(
-            user.get("name"), len(completed), len(todos)))
-
-        # Print the completed tasks one by one with indentation
-        [print("\t{}".format(complete)) for complete in completed]
-
-    except IndexError:
-        print("Please provide an employee ID as a command-line argument.")
-    except requests.exceptions.RequestException as e:
-        print(f"Error accessing the API: {e}")
+    # Print the completed tasks one by one with indentation
+    [print("\t {}".format(complete)) for complete in completed]
 
