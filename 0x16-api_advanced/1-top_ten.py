5ce78af18ceb63e28edd6c2dc0b661f676ded895
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2024-08-20 21:12:08 -0700

    reburn

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,20 +2,18 @@
 """ Script to get the first 10 hot
     posts on Reddit
 """
-from requests import get
-
+import requests
 
 def top_ten(subreddit):
-    """get first 10 hot post for a subreddit"""
-    if subreddit and type(subreddit) is str:
-        url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-        headers = {'user-agent': 'my-app/0.0.1'}
-        params = {'limit': 10}
-        req = get(url, params=params, headers=headers, allow_redirects=False)
-        if req.status_code == 200:
-            data = req.json()
-            posts = data.get('data', {}).get('children', {})
-            for post in posts:
-                print(post.get('data').get('title'))
-        else:
-            print(None)
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    posts = response.json().get('data', {}).get('children', [])
+    for post in posts:
+        print(post['data']['title'])
+
